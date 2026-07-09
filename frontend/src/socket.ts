6ef58aad59930e20ba50764cc8472977b781ed2a
rugveda <rugveda@rugvedas-MacBook.local>
2026-07-09 06:59:49 +0530

    fix socket production url

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   autoConnect: false,
 });
 
 export default socket;
-
